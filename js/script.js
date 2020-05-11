c104cf60f56b830221fce6d5d9a43fd6eb715135
ewoxone <ewoxone@yandex.ru>
2020-05-11 18:35:42 +0300

    1 задание

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-let human = {
-    name: 'John',
-    age: '25',
-    isMarried: true
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-console.log(`Вас зовут ${human.name} и вам ${human.age} лет`);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b; 
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
